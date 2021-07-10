9a2495a699b8d3ef0a513f2dc66e2c69be24d87a
co3oing <co3oing@naver.com>
2021-07-10 17:13:36 +0900

    Compose the Detail View

--- Landmarks/CircleImage.swift
+++ Landmarks/CircleImage.swift
@@ -10,6 +10,8 @@ import SwiftUI
 struct CircleImage: View {
     var body: some View {
         Image("turtlerock")
+            .resizable()
+            .frame(width: 250, height: 250, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
             .clipShape(Circle())
             .overlay(Circle().stroke(Color.white, lineWidth: 4))
             .shadow(radius: 7)
